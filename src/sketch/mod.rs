eb27e0396cc9c0f07c41510777154e6c0e046e07
John Lees <lees.john6@gmail.com>
2026-08-03 12:07:29 +0100

    Make some more sketch fields public

--- src/sketch/mod.rs
+++ src/sketch/mod.rs
@@ -131,12 +131,18 @@ pub struct Sketch {
     usigs: SketchVec,
     name: String,
     index: Option<usize>,
-    rc: bool,
-    reads: bool,
-    seq_length: usize,
-    densified: bool,
-    acgt: [usize; 4],
-    non_acgt: usize,
+    /// Sequence and reverse complement used
+    pub rc: bool,
+    /// Built from reads (fastq)
+    pub reads: bool,
+    /// Assembled sequence length (fasta: exact; fastq: estimate)
+    pub seq_length: usize,
+    /// Sketch required densification
+    pub densified: bool,
+    /// Number of [A, C, G, T] bases
+    pub acgt: [usize; 4],
+    /// Number of non-ACGT bases
+    pub non_acgt: usize,
 }
 
 impl Default for Sketch {
